02622ac40bbba6d90d2107be1a97813d5256c6a5
emanuel lopez <emanuel-lopez13@hotmail.com>
2022-12-22 20:22:28 -0300

    Añadiendo validaciones a los formularios de Registro e Login  con Fromik y Yup

--- components/auth/Login.jsx
+++ components/auth/Login.jsx
@@ -1,34 +1,94 @@
-import React from "react";
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = ({ showRegisterForm }) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      password: "",
+      email: "",
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(10, "Must be 15 characters or less")
+        .min(5, "faltan caracteres")
+        .required("Name requerido"),
+
+      password: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .min(6, "faltan caracteres")
+        .required("Password requerido"),
+
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email requerido"),
+    }),
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="h-[100px] w-[300px] md:h-[200px] md:w-[350px] rounded-xl text-white">
       <div className=" mx-auto ">
         <h1 className="text-3xl text-black font-bold">Login</h1>
 
-        <form className="w-full flex flex-col py-4">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="w-full flex flex-col py-4"
+        >
           <input
             className="p-3 my-2  bg-gray-700 rounded"
+            id="name"
             type="text"
             placeholder="Nombre"
+            name="name"
             autoComplete="nombre"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
           />
-
+          {formik.touched.name && formik.errors.name ? (
+            <div className="text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.name}
+            </div>
+          ) : null}
           <input
             className="p-3 my-2  bg-gray-700 rounded"
+            id="email"
             type="text"
             placeholder="Email"
             autoComplete="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
-
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.email}
+            </div>
+          ) : null}
           <input
             id="password"
             type="password"
             placeholder="Password"
             className="p-3 my-2 bg-gray-700 rouded"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
           />
-
-          <button className="bg-[#E58D27] text-black py-3  my-6 rounded font-bold">
+          {formik.touched.password && formik.errors.password ? (
+            <div className=" text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.password}
+            </div>
+          ) : null}
+          <button
+            type="submit"
+            className="bg-[#E58D27] text-black py-3 my-6 rounded font-bold"
+          >
             Iniciar Sesion
           </button>
           <div className="flex justify-between items-center text-sm text-gray-600">
--- components/auth/Register.jsx
+++ components/auth/Register.jsx
@@ -1,39 +1,114 @@
-import React from "react";
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Register = ({ showLoginForm }) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      password: "",
+      email: "",
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(10, "Must be 15 characters or less")
+        .min(5, "faltan caracteres")
+        .required("Name requerido"),
+      lastname: Yup.string()
+        .max(10, "Must be 15 characters or less")
+        .min(5, "faltan caracteres")
+        .required("apellido requerido"),
+      password: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .min(6, "faltan caracteres")
+        .required("Password requerido"),
+
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email requerido"),
+    }),
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className="h-[100px] w-[300px] md:h-[200px] md:w-[350px] rounded-xl text-white">
       <div className=" mx-auto ">
         <h1 className="text-3xl text-black font-bold">Registrarse</h1>
 
-        <form className="w-full flex flex-col py-4">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="w-full flex flex-col py-4"
+        >
           <input
             className="p-3 my-2  bg-gray-700 rounded"
+            id="name"
             type="text"
             placeholder="Nombre"
+            name="name"
             autoComplete="nombre"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
           />
+          {formik.touched.name && formik.errors.name ? (
+            <div className="text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.name}
+            </div>
+          ) : null}
 
           <input
             className="p-3 my-2  bg-gray-700 rounded"
-            type="email"
+            id="lastname"
+            type="text"
+            placeholder="Apellido"
+            name="lastname"
+            autoComplete="nombre"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastname}
+          />
+          {formik.touched.lastname && formik.errors.lastname ? (
+            <div className="text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.lastname}
+            </div>
+          ) : null}
+
+          <input
+            className="p-3 my-2  bg-gray-700 rounded"
+            id="email"
+            type="text"
             placeholder="Email"
             autoComplete="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.email}
+            </div>
+          ) : null}
 
           <input
             id="password"
             type="password"
             placeholder="Password"
             className="p-3 my-2 bg-gray-700 rouded"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
           />
-          <input
-            id="password2"
-            type="password"
-            placeholder="Repetir tu Password"
-            className="p-3 my-2 bg-gray-700 rouded"
-          />
-          <button className="bg-[#E58D27] text-black py-3  my-6 rounded font-bold">
+          {formik.touched.password && formik.errors.password ? (
+            <div className=" text-[#8d2d2c] font-bold text-sm">
+              {formik.errors.password}
+            </div>
+          ) : null}
+          <button type="submit" className="bg-[#E58D27] text-black py-3  my-6 rounded font-bold">
             Registrarse
           </button>
           <div className="flex justify-between items-center text-sm text-gray-600">
@@ -43,10 +118,10 @@ const Register = ({ showLoginForm }) => {
             </p>
           </div>
           <p className="py-10 mb-2 ">
-            <span className="text-gray-600">
-              Already subscribed?
-            </span>{" "}
-            <button onClick={showLoginForm} className="text-[#E58D27]" >Iniciar sesion</button>
+            <span className="text-gray-600">Already subscribed?</span>{" "}
+            <button onClick={showLoginForm} className="text-[#E58D27]">
+              Iniciar sesion
+            </button>
           </p>
         </form>
       </div>
